3adef31bef2b4baebd68f64a33b6ab0e82508417
Elizabethyonas <131787071+Elizabethyonas@users.noreply.github.com>
2023-05-20 21:41:16 +0300

    Update fizzbuzz.py

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,1 +1,14 @@
+class Solution:
+    def fizzBuzz(self, n: int) -> List[str]:
+        FinalList = []
 
+        for i in range (1,n + 1):
+            if i % 3 == 0 and i % 5 == 0:
+                FinalList.append("FizzBuzz")
+            elif i % 3 == 0:
+                FinalList.append("Fizz")
+            elif i % 5 == 0:
+                FinalList.append("Buzz")
+            else:
+                FinalList.append(str(i))
+        return FinalList
