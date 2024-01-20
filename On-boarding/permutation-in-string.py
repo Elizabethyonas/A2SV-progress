287b386b8c1a77979bad44c14e9c66c11970eae9
Elizabethyonas <131787071+Elizabethyonas@users.noreply.github.com>
2024-01-20 22:42:03 +0300

    Added solution for Permutation in String

---
+++ On-boarding/permutation-in-string.py
@@ -0,0 +1,27 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        k=len(s1)
+        dic_s2={}
+        dic_s1={}
+        l=0
+        for char in set(s1+s2):
+            dic_s2[char]=0
+            dic_s1[char]=0
+        for char in s1:
+            dic_s1[char]+=1
+        if len(s2)<k:
+            return False
+        for i in range(k):
+            dic_s2[s2[i]]+=1
+        if dic_s2==dic_s1:
+                return True
+        for i in range(k,len(s2)):
+            dic_s2[s2[l]]-=1
+            l+=1
+            dic_s2[s2[i]]+=1
+            if dic_s2==dic_s1:
+                return True 
+        return False
+
+
+            
