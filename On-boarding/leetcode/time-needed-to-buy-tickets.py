3e1a74bbea3fd341e5c89663211e9d517f6b430f
Elizabethyonas <131787071+Elizabethyonas@users.noreply.github.com>
2024-02-29 10:21:20 +0300

    Add solution for Time Needed to Buy Tickets

---
+++ On-boarding/leetcode/time-needed-to-buy-tickets.py
@@ -0,0 +1,16 @@
+class Solution:
+    def timeRequiredToBuy(self, tickets: List[int], k: int) -> int:
+        n = tickets[k]
+        time = 0
+        while tickets[k] != 0:
+            for i in range(len(tickets)):
+                
+                if tickets[i] != 0:
+                    time += 1
+                    tickets[i] = tickets[i] - 1
+                if tickets[k] == 0:
+                    return time
+
+        if tickets[k] == 0:
+            return time 
+        
